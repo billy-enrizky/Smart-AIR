9529253d57fac48be53ce06f6c9a26d9b93fab0e
Muhammad Enrizky Brillian <132111170+billy-enrizky@users.noreply.github.com>
2025-11-08 15:56:17 -0500

    Revert "Feature/no ref/firebase sdk setup"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application)
 }
 
 android {
@@ -33,9 +32,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-database")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
