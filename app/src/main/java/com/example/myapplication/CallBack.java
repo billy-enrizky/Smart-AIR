2ea6ba0c31a9fbe5bf81823937aef66f04a2d1e0
Yipeng Zhao <passione158@gmail.com>
2025-11-16 22:43:47 -0500

    feat: add On-Boarding Tutorial page and read info from database function

--- app/src/main/java/com/example/myapplication/CallBack.java
+++ app/src/main/java/com/example/myapplication/CallBack.java
@@ -1,5 +1,10 @@
 package com.example.myapplication;
 
 public interface CallBack {
+    /* this CallBack interface is used for synchronize the progress of the apps and database.
+    Otherwise, the asynchronization cause bugs.
+    For example, using UserManager.currentUser.firstTime before the reading is done would cause bug.
+    Callback make it possible that the code would executed only after the reading is done.
+    */
     void onComplete();
 }
