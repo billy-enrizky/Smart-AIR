15097d7dc71f5f9cc7730bd16a214ed6956c3356
Kingsley Chan <chankingsleykt@gmail.com>
2025-11-22 16:45:06 -0500

    finished merging changes with main branch

--- app/src/main/java/com/example/myapplication/userdata/ParentAccount.java
+++ app/src/main/java/com/example/myapplication/userdata/ParentAccount.java
@@ -11,25 +11,43 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class ParentAccount extends UserData {
-    ArrayList<String> Children_id;
+public class ParentAccount extends UserData implements Cloneable {
     String Email;
+    HashMap<String, Object> children;
+    InviteCode InviteCode;
+    ArrayList<String> LinkedProvidersId;
+
     public ParentAccount() {
         super();
         Email = "";
-        Children_id = new ArrayList<String>();
+        this.children = new HashMap<String, Object>();
+        this.LinkedProvidersId = new ArrayList<String>();
         Account = AccountType.PARENT;
     }
     public ParentAccount(String ID, String Email) {
         super(ID);
         this.Email = Email;
         this.Account = AccountType.PARENT;
-        Children_id = new ArrayList<String>();
+        this.children = new HashMap<String, Object>();
+        this.LinkedProvidersId = new ArrayList<String>();
     }
 
-    public void addChild(String id) {
-        Children_id.add(id);
+    public ParentAccount(ParentAccount other) {
+        this.Email = other.Email;
+        this.Account = AccountType.PARENT;
+        this.LinkedProvidersId = new ArrayList<String>();
+
+    }
+
+    public void addChild(String username, String name, String dob, String age, String notes) {
+        HashMap<String, String>childData = new HashMap<String, String>();
+        childData.put("name", name);
+        childData.put("dob", dob);
+        childData.put("age", age);
+        childData.put("notes", notes);
+        this.children.put(username, childData);
     }
     public void setEmail(String Email){
         this.Email = Email;
@@ -37,12 +55,25 @@ public class ParentAccount extends UserData {
     public String getEmail(){
         return Email;
     }
-    public ArrayList<String> getChildrenid(){
-        return Children_id;
+    public HashMap<String, Object> getChildren(){
+        return this.children;
     }
 
-    public void setChildrenid(ArrayList<String> Children_id){
-        this.Children_id = Children_id;
+    public void setInviteCode(InviteCode InviteCode){
+        this.InviteCode = InviteCode;
+    }
+    public InviteCode getInviteCode(){
+        return InviteCode;
+    }
+    public void setLinkedProvidersId(ArrayList<String> LinkedProviders){
+        this.LinkedProvidersId = LinkedProviders;
+    }
+    public ArrayList<String> getLinkedProvidersId(){
+        return LinkedProvidersId;
+    }
+
+    public void addLinkedProvider(String ID){
+        LinkedProvidersId.add(ID);
     }
 
     @Override
@@ -56,13 +87,12 @@ public class ParentAccount extends UserData {
                 else {
                     DataSnapshot Snapshot = task.getResult();
                     ParentAccount Data = Snapshot.getValue(ParentAccount.class);
-                    //     UserManager.mDatabase.child("test").setValue(Data.childrenid);
                     ParentAccount.this.ID = Data.ID;
                     ParentAccount.this.Account = Data.Account;
                     ParentAccount.this.FirstTime = Data.FirstTime;
                     ParentAccount.this.Email = Data.Email;
-                    ParentAccount.this.Children_id = Data.Children_id;
-                    //UserManager.mDatabase.child("test").setValue(ParentAccount.this.Children_id);
+                    ParentAccount.this.children = Data.children;
+                    ParentAccount.this.LinkedProvidersId = Data.LinkedProvidersId;
                     if(callback != null){
                         callback.onComplete();
                     }
@@ -71,4 +101,3 @@ public class ParentAccount extends UserData {
         });
     }
 }
-
