196847571f920db0cb8b31b60a2cdd00e2609f29
Yipeng Zhao <passione158@gmail.com>
2025-11-21 20:00:23 -0500

    chore: save current work

--- app/src/main/java/com/example/myapplication/SignIn/SignInModel.java
+++ app/src/main/java/com/example/myapplication/SignIn/SignInModel.java
@@ -1,13 +1,18 @@
 package com.example.myapplication.SignIn;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.example.myapplication.CallBack;
+import com.example.myapplication.ResultCallBack;
 import com.example.myapplication.UserManager;
+import com.example.myapplication.userdata.UserData;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -15,16 +20,46 @@ public class SignInModel {
 
     public static FirebaseAuth mAuth = FirebaseAuth.getInstance();
     public static DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-    static void SignIn(String email, String password, CallBack callBack) {
+    static void AuthSignIn(String email, String password, ResultCallBack<Boolean> callBack) {
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 // if successful, notify user and return to main page.
                 if(task.isSuccessful()){
                     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-                    UserManager.currentUser.ReadFromDatabase(mAuth.getCurrentUser().getUid(),null);
+                    UserManager.currentUser.ReadFromDatabase(mAuth.getCurrentUser().getUid(),new CallBack() {
+                        @Override
+                        public void onComplete() {
+                            if(callBack != null){
+                                callBack.onComplete(true);
+                            }
+                        }
+                    });
                 }else{
-                    callBack.onComplete();
+                    if(callBack != null){
+                        callBack.onComplete(false);
+                    }
+                }
+            }
+        });
+    }
+    static String AuthGetCurrentUID(){
+        return mAuth.getCurrentUser().getUid();
+    }
+
+    static void DataBaseRead(String ID, ResultCallBack<UserData> callBack){
+        mDatabase.child("users").child(ID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    DataSnapshot Snapshot = task.getResult();
+                    UserData Data = Snapshot.getValue(UserData.class);
+                    if(callBack != null){
+                        callBack.onComplete(Data);
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/myapplication/SignIn/SignInPresenter.java
+++ app/src/main/java/com/example/myapplication/SignIn/SignInPresenter.java
@@ -1,18 +1,52 @@
 package com.example.myapplication.SignIn;
 
+import android.content.Intent;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication.CallBack;
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.OnBoardingActivity;
+import com.example.myapplication.ResetPasswordActivity;
+import com.example.myapplication.ResultCallBack;
+import com.example.myapplication.SignUpActivity;
+import com.example.myapplication.UserManager;
+import com.example.myapplication.userdata.UserData;
 
 public class SignInPresenter {
     static void signIn(AppCompatActivity activity, String email, String password) {
-        SignInModel.SignIn(email, password, new CallBack() {
+        SignInModel.AuthSignIn(email, password, new ResultCallBack<Boolean>() {
             @Override
-            public void onComplete() {
-                Toast.makeText(activity, "User Not found!", Toast.LENGTH_SHORT).show();
+            public void onComplete(Boolean result) {
+                if(result){
+                    Toast.makeText(activity, "Welcome!", Toast.LENGTH_SHORT).show();
+                    String ID = SignInModel.AuthGetCurrentUID();
+                    SignInModel.DataBaseRead(ID, new ResultCallBack<UserData>() {
+                        @Override
+                        public void onComplete(UserData result) {
+                            UserManager.currentUser = result;
+                            if(UserManager.currentUser.getFirstTime()){
+                                Intent intent1 = new Intent(activity, OnBoardingActivity.class);
+                                activity.startActivity(intent1);
+                            }else{
+                                Intent intent1 = new Intent(activity, MainActivity.class);
+                                activity.startActivity(intent1);
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(activity, "User Not found!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    public void CreateNewAccount(AppCompatActivity activity) {
+        Intent intent = new Intent(activity, SignUpActivity.class);
+        activity.startActivity(intent);
+    }
+
+    public void forgotPassword(AppCompatActivity activity) {
+        Intent intent = new Intent(activity, ResetPasswordActivity.class);
+        activity.startActivity(intent);
     }
 }
