40cbd0f930a291afcaaa87ad9b172066555de823
Yipeng Zhao <passione158@gmail.com>
2025-11-21 19:38:01 -0500

    chore: save current work

--- app/src/main/java/com/example/myapplication/SignIn/SignInModel.java
+++ app/src/main/java/com/example/myapplication/SignIn/SignInModel.java
@@ -1,4 +1,32 @@
 package com.example.myapplication.SignIn;
 
+import androidx.annotation.NonNull;
+
+import com.example.myapplication.CallBack;
+import com.example.myapplication.UserManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignInModel {
+
+    public static FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public static DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    static void SignIn(String email, String password, CallBack callBack) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // if successful, notify user and return to main page.
+                if(task.isSuccessful()){
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                    UserManager.currentUser.ReadFromDatabase(mAuth.getCurrentUser().getUid(),null);
+                }else{
+                    callBack.onComplete();
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/myapplication/SignIn/SignInPresenter.java
+++ app/src/main/java/com/example/myapplication/SignIn/SignInPresenter.java
@@ -1,4 +1,18 @@
 package com.example.myapplication.SignIn;
 
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.CallBack;
+
 public class SignInPresenter {
+    static void signIn(AppCompatActivity activity, String email, String password) {
+        SignInModel.SignIn(email, password, new CallBack() {
+            @Override
+            public void onComplete() {
+                Toast.makeText(activity, "User Not found!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
