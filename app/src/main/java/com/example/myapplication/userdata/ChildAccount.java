ba97b911f3c0bac20fbd6a950ed4011a9ac669d7
Yipeng Zhao <passione158@gmail.com>
2025-11-22 18:39:36 -0500

    chore: save current work

--- app/src/main/java/com/example/myapplication/userdata/ChildAccount.java
+++ app/src/main/java/com/example/myapplication/userdata/ChildAccount.java
@@ -1,13 +1,35 @@
 package com.example.myapplication.userdata;
 
+import com.example.myapplication.userdata.selectoritem.SharedItems;
+
 public class ChildAccount extends UserData {
     String Parent_id;
-    public ChildAccount() {
-        super();
-        Parent_id = "";
-    }
+    String dob;
+    int age;
+    SharedItems SharedItems;
     public ChildAccount(String ID) {
         super(ID);
-        Parent_id = "";
+    }
+    public ChildAccount() {
+        super();
+    }
+    public ChildAccount(String ID, String Parent_id, String dob, int age) {
+        this.ID = ID;
+        this.Parent_id = Parent_id;
+        this.dob = dob;
+        this.age = age;
+    }
+    public void setDob(int year, int month, int day) {
+        this.dob = ""+year+"/" + month + "/" + day;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSharedItems(SharedItems SharedItems) {
+        this.SharedItems = SharedItems;
+    }
+    public SharedItems getSharedItems() {
+        return SharedItems;
     }
 }
