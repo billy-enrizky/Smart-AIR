5a8d420047db0d7407f0bb9552aa41e2c6f49c42
Yipeng Zhao <passione158@gmail.com>
2025-11-22 18:39:36 -0500

    chore: save current work

---
+++ app/src/main/java/com/example/myapplication/userdata/selectoritem/SharedItems.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.userdata.selectoritem;
+
+public class SharedItems {
+    Boolean rescueLogs;
+    Boolean controllerAdherenceSummary;
+    Boolean symptoms;
+    Boolean triggers;
+    Boolean PEF;
+    Boolean triageIncidents;
+    Boolean summaryCharts;
+
+    public SharedItems(){
+        rescueLogs = false;
+        controllerAdherenceSummary = false;
+        symptoms = false;
+        triggers = false;
+        PEF = false;
+        triageIncidents = false;
+        summaryCharts = false;
+    }
+
+}
