39e958fbebe7d1214896fab2b8167d875da7dac1
Yipeng Zhao <passione158@gmail.com>
2025-11-23 23:20:37 -0500

    feat: add related database change

--- app/src/main/java/com/example/myapplication/dailycheckin/DailyCheckin.java
+++ app/src/main/java/com/example/myapplication/dailycheckin/DailyCheckin.java
@@ -3,10 +3,15 @@ package com.example.myapplication.dailycheckin;
 public class DailyCheckin {
     String username;
     String yet_to_implement;
-    // mind that, to have setValue(this) works, a pair of getter setter is needed
+    // mind that, to have setValue(this) works, a pair of getter setter is needed for each field
     // a constructor of empty parameter is also needed.
     public DailyCheckin() {
     }
-
+    public String getUsername(){ // format of getName, mind the upper case
+        return username;
+    }
+    public void setUsername(String username){ // getName
+        this.username = username;
+    }
 
 }
