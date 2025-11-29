591cb37b67dfd7b90ce738512e74bb946df55d06
Yipeng Zhao <passione158@gmail.com>
2025-11-29 13:51:16 -0500

    chore: save work

--- app/src/main/java/com/example/myapplication/providers/ProviderActivity.java
+++ app/src/main/java/com/example/myapplication/providers/ProviderActivity.java
@@ -9,20 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapplication.CallBack;
 import com.example.myapplication.R;
 import com.example.myapplication.SignIn.SignInView;
 import com.example.myapplication.UserManager;
 import com.example.myapplication.providermanaging.InvitationAcceptActivity;
-import com.example.myapplication.userdata.ChildAccount;
-import com.example.myapplication.userdata.ParentAccount;
-import com.example.myapplication.userdata.ProviderAccount;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.ArrayList;
-
 public class ProviderActivity extends AppCompatActivity {
-    ProviderAccount currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +28,17 @@ public class ProviderActivity extends AppCompatActivity {
             return insets;
         });
         UserManager.isProviderAccount(this);
-        currentUser = (ProviderAccount) UserManager.currentUser;
-        ArrayList<String> LinkedParentsId = currentUser.getLinkedParentsId();
-        ArrayList<ChildAccount> LinkedChildren = new ArrayList<>();
-        for(int i = 0; i < LinkedParentsId.size(); i++) {
-            String ParentID = LinkedParentsId.get(i);
-            ParentAccount parent = new ParentAccount();
-            parent.ReadFromDatabase(ParentID, new CallBack() {
-                @Override
-                public void onComplete() {
-                    LinkedChildren.addAll(parent.getChildren().values());
-                }
-            });
-        }
     }
     public void Signout(android.view.View view){
         UserManager.currentUser = null;
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, SignInView.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+    public void AccessInfo(android.view.View view){
+        Intent intent = new Intent(this, AccessInfoActivity.class);
         startActivity(intent);
         this.finish();
     }
